e09c915c181e270e309f3df4545cad6b507a2217
Qing Miao <cdutwhu@outlook.com>
2020-01-03 11:46:50 +1100

    init version, [common + preprocess + jkv]

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ./preprocess && ./clean.sh && cd ..
+cd ./jkv && ./clean.sh && cd ..
---
+++ common/var.go
@@ -0,0 +1,30 @@
+package common
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var (
+	fPf         = fmt.Printf
+	fPln        = fmt.Println
+	fSf         = fmt.Sprintf
+	fSp         = fmt.Sprint
+	fEf         = fmt.Errorf
+	sSpl        = strings.Split
+	sJoin       = strings.Join
+	sCount      = strings.Count
+	sReplace    = strings.Replace
+	sReplaceAll = strings.ReplaceAll
+	sIndex      = strings.Index
+	sLastIndex  = strings.LastIndex
+	sTrim       = strings.Trim
+	sTrimLeft   = strings.TrimLeft
+	sHasPrefix  = strings.HasPrefix
+	sHasSuffix  = strings.HasSuffix
+
+	RExpMD5    = regexp.MustCompile("\"[a-f0-9]{32}\"")
+	RExpSHA1   = regexp.MustCompile("\"[a-f0-9]{40}\"")
+	RExpSHA256 = regexp.MustCompile("\"[a-f0-9]{64}\"")
+)
