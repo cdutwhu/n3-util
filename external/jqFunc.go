2a9dd146aa4588857d6ddd1300c82b283538d96e
Qing Miao <cdutwhu@outlook.com>
2020-05-08 16:49:29 +1000

    * external

--- external/jqFunc.go
+++ external/jqFunc.go
@@ -4,27 +4,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-
-	eg "github.com/cdutwhu/json-util/n3errs"
 )
-
-// Prepare :
-func Prepare(exe string, wkDirs ...string) (cWD, oriWD string, err error) {
-	oriWD, err = os.Getwd()
-	failOnErr("%v", err)
-	wkDirs = append(wkDirs, "./", "../", "../../")
-	for _, cWD = range wkDirs {
-		cWD = sTrimRight(cWD, "/") + "/"
-		if _, err = os.Stat(cWD + exe); err == nil {
-			failOnErr("%v", os.Chdir(cWD))
-			cWD, err = os.Getwd()
-			failOnErr("%v", err)
-			return cWD, oriWD, nil
-		}
-	}
-	failOnErr("%v: %s", eg.FILE_NOT_FOUND, exe)
-	return "", "", nil
-}
 
 // FmtJSONStr : May have 'Argument list too long' issue !
 func FmtJSONStr(json string, jqDirs ...string) string {
---
+++ external/prepare.go
@@ -0,0 +1,25 @@
+package external
+
+import (
+	"os"
+
+	eg "github.com/cdutwhu/json-util/n3errs"
+)
+
+// Prepare : // append `defer func() { os.Chdir(oriWD) }()` after this function
+func Prepare(exe string, wkDirs ...string) (cWD, oriWD string, err error) {
+	oriWD, err = os.Getwd()
+	failOnErr("%v", err)
+	wkDirs = append(wkDirs, "./", "../", "../../")
+	for _, cWD = range wkDirs {
+		cWD = sTrimRight(cWD, "/") + "/"
+		if _, err = os.Stat(cWD + exe); err == nil {
+			failOnErr("%v", os.Chdir(cWD))
+			cWD, err = os.Getwd()
+			failOnErr("%v", err)
+			return cWD, oriWD, nil
+		}
+	}
+	failOnErr("%v: %s", eg.FILE_NOT_FOUND, exe)
+	return "", "", nil
+}
